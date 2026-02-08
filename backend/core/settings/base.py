2e930c9ce153dfc9709bfa273eeb4738f142be7b
Tony Souza <tonysouz7@gmail.com>
2026-02-08 05:36:15 -0300

    fix: corrige caminho do WSGI_APPLICATION

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -63,7 +63,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
